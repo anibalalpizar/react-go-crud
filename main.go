a4ccbe4260fa7e06e1f4ce391e42d434a2ad96e4
anibalalpizar <anibal.alpizar14@gmail.com>
2025-01-17 20:08:39 -0600

    add POST endpoint for creating todos with validation

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
-    app := fiber.New()
+	app := fiber.New()
+	todos := []Todo{}
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.Status(200).JSON(fiber.Map{"message": "Hello, World!"})
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"message": "Hello, World!"})
+	})
 
-    log.Fatal(app.Listen(":3000"))
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := Todo{}
+
+		if err := c.BodyParser(&todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Body is required"})
+		}
+
+        todo.ID = len(todos) + 1
+        todos = append(todos, todo)
+
+        return c.Status(201).JSON(todo)       
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
