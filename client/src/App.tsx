2ef5b16481e9e4f892a70d3c9f5fcd9eb39b9603
anibalalpizar <anibal.alpizar14@gmail.com>
2025-01-26 20:20:29 -0600

    feat: centralize API base URL for improved maintainability in TodoList component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar"
 import TodoForm from "./components/TodoForm"
 import TodoList from "./components/TodoList"
 
+export const BASE_URL = "http://localhost:3000/api"
+
 function App() {
   return (
     <Stack h="100vh">
--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -2,6 +2,7 @@ import { Flex, Spinner, Stack, Text } from "@chakra-ui/react"
 import { useQuery } from "@tanstack/react-query"
 
 import TodoItem from "./TodoItem"
+import { BASE_URL } from "../App"
 
 export type Todo = {
   _id: number
@@ -14,7 +15,7 @@ const TodoList = () => {
     queryKey: ["todos"],
     queryFn: async () => {
       try {
-        const res = await fetch("http://localhost:3000/api/todos")
+        const res = await fetch(BASE_URL + "/todos")
         const data = await res.json()
 
         if (!res.ok) throw new Error(data.error || "Something went wrong!")
