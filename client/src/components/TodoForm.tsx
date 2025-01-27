34883946fe037269a8ca34fe51bf44cade4facde
anibalalpizar <anibal.alpizar14@gmail.com>
2025-01-26 20:30:24 -0600

    feat: implement create functionality for Todo items with error handling and loading state

--- client/src/components/TodoForm.tsx
+++ client/src/components/TodoForm.tsx
@@ -1,15 +1,38 @@
 import { useState } from "react"
 import { Button, Flex, Input, Spinner } from "@chakra-ui/react"
 import { IoMdAdd } from "react-icons/io"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+
+import { BASE_URL } from "../App"
 
 const TodoForm = () => {
   const [newTodo, setNewTodo] = useState("")
-  const [isPending, setIsPending] = useState(false)
 
-  const createTodo = async (e: React.FormEvent) => {
-    e.preventDefault()
-    alert("Todo added!")
-  }
+  const queryClient = useQueryClient()
+  const { mutate: createTodo, isPending: isCreating } = useMutation({
+    mutationKey: ["createTodo"],
+    mutationFn: async (event: React.FormEvent) => {
+      event.preventDefault()
+
+      try {
+        const res = await fetch(BASE_URL + "/todos", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ body: newTodo }),
+        })
+        const data = await res.json()
+        if (res.ok) throw new Error(data.error || "Failed to create todo")
+        setNewTodo("")
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+      }
+    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["todos"] }),
+    onError: (error) => alert(error.message),
+  })
   return (
     <form onSubmit={createTodo}>
       <Flex gap={2}>
@@ -26,7 +49,7 @@ const TodoForm = () => {
             transform: "scale(.97)",
           }}
         >
-          {isPending ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
+          {isCreating ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
         </Button>
       </Flex>
     </form>
