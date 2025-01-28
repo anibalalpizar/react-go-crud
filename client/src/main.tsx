e2245dc905ed6ba2a2b68b9d6d3c2bc1d606ba4d
anibalalpizar <anibal.alpizar14@gmail.com>
2025-01-27 21:23:25 -0600

    feat: add custom Chakra UI theme with dark mode support

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,12 @@ import { ChakraProvider } from "@chakra-ui/react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import App from "./App.tsx"
+import theme from "./chakra/theme.ts"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={new QueryClient()}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </QueryClientProvider>
